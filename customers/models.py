f3a9fb3ab08b9652be0d03a693b124f98ee4d01b
Bui Anh Tai <75308243+buianhtai1205@users.noreply.github.com>
2022-04-28 17:00:55 +0700

    create table customer

---
+++ customers/models.py
@@ -0,0 +1,19 @@
+from __future__ import unicode_literals
+from django.db import models
+
+# Create your models here.
+from uuid import uuid4
+
+class Customer(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50, unique=True)
+    password = models.CharField(max_length=200)
+    phone = models.CharField(max_length=15)
+    address = models.CharField(max_length=200)
+    token = models.CharField(max_length=200, blank=True, editable=False, default=uuid4())
+
+    def __unicode__(self):
+        return self.content
+    
+    def __str__(self):
+        return self.name
--- products/admin.py
+++ products/admin.py
@@ -7,6 +7,6 @@ class SettingAdmin(admin.ModelAdmin):
     list_display_links = ('id', 'name', 'product_img', 'price', 'manufacturer_id')
     list_per_page = 5
     sortable_by = ('id')
-    list_filter = ('status', 'manufacturer_id')
+    list_filter = ('manufacturer_id', )
 
 admin.site.register(Product, SettingAdmin)
