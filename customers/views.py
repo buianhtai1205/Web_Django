895728ec5df689c2a9a2463c79a58388521a4c97
Bui Anh Tai <75308243+buianhtai1205@users.noreply.github.com>
2022-04-28 20:34:56 +0700

    update link two form

--- customers/views.py
+++ customers/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Customer
+
+def signin(request):
+    return render(request, "customers/form_signin.html")
+
+def signup(request):
+    return render(request, "customers/form_signup.html")
