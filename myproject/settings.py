26f362dc131682b4e7b701c6ea2e749b7730ea82
duytaovo <duytaovo@gmail.com>
2022-05-04 10:21:38 +0700

    first

--- myproject/settings.py
+++ myproject/settings.py
@@ -15,7 +15,8 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+STATIC_ROOT = "Web_Django/products/static";
+LOGIN_URL = 'chart'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
