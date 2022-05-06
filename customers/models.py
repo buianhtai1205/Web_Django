552a5a9e54e70ea6afcbbaea6d1af7a62d68065f
Bui Anh Tai <75308243+buianhtai1205@users.noreply.github.com>
2022-05-06 23:34:19 +0700

    update sign out, session

--- customers/models.py
+++ customers/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # Create your models here.
 from uuid import uuid4
 
+
 class Customer(models.Model):
     name = models.CharField(max_length=50)
     email = models.EmailField(max_length=50, unique=True)
@@ -17,3 +18,5 @@ class Customer(models.Model):
     
     def __str__(self):
         return self.name
+
+   
--- customers/views.py
+++ customers/views.py
@@ -1,22 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from django.http import HttpResponse
 from .models import Customer
 from products.models import Product
 from .forms import CreateCustomersForm, SigninCustomersForm
+from django.urls import reverse
 from django.http import JsonResponse
 from django.core.serializers import serialize
 
 
 def index(request):
+    content = {}
+    if request.session._session:
+        if (request.session['username']):
+            username = request.session['username']
+            content['username'] = username
     products_iphone = Product.objects.filter(manufacturer_id = 15)
-    content = {'ds_product_iphone': products_iphone}
+    content['ds_product_iphone'] = products_iphone
     return render(request, 'customers/index.html', content)
 
 def signin(request):
     a = SigninCustomersForm()
     return render(request, "customers/form_signin.html", {'form' : a})
+
+def signout(request):
+    del request.session['username']
+    return redirect(reverse('customers:index', kwargs = {}))
 
 def signup(request):
     a = CreateCustomersForm()
@@ -26,8 +36,11 @@ def saveCustomer(request):
     if request.method == 'POST':
         a = CreateCustomersForm(request.POST)
         if a.is_valid():
+            email = request.POST.dict()['email']
+            username = email.split('@')[0]
+            request.session['username'] = username
             a.save()
-            return render(request, 'customers/index.html', {'title':'index'})
+            return redirect(reverse('customers:index', kwargs = {}))
 
 def checkCustomer(request):
     if request.method == 'POST':
@@ -36,10 +49,15 @@ def checkCustomer(request):
         password = login_data['password']
         num_rows = Customer.objects.filter(email=email, password=password).count()
         if num_rows > 0:
-            return render(request, 'customers/index.html', {'title':'index'})
+            username = email.split('@')[0]
+            request.session['username'] = username
+            messages = "Đăng nhập thành công!"
+            content = {'username': username, 'messages': messages}
+            return redirect(reverse('customers:index', kwargs = {}))
         else:
-            a = SigninCustomersForm
-            return render(request, "customers/form_signin.html", {'form' : a})
+            messages = "Email hoặc mật khẩu không đúng!"
+            content = {'messages': messages}
+            return redirect('customers:signin')
     return HttpResponse("Sai method")
 
 
