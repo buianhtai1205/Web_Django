aba5e2260e82a6099f7b99edf01451f31253da6b
Bui Anh Tai <75308243+buianhtai1205@users.noreply.github.com>
2022-05-09 19:24:29 +0700

    fix error

--- customers/views.py
+++ customers/views.py
@@ -14,7 +14,7 @@ from django.core import serializers
 
 def index(request):
     content = {}
-    if request.session._session:
+    if 'username' in request.session:
         if (request.session['username']):
             username = request.session['username']
             content['username'] = username
